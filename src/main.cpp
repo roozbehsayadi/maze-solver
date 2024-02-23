dd77db00f514d55ab055a08a3731b490fcd8657e
Roozbeh <roozbehsayadi@gmail.com>
2024-02-23 13:23:20 +0330

    Fix CMakeLists file to copy the assets into the build folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
 
 #include <iostream>
+#include <filesystem>
 
 #include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 int main() {
-	cv::Mat img;
+	const auto imagePath = "./assets/maze_1.jpg";
+
+	cv::Mat img = cv::imread(imagePath, cv::IMREAD_COLOR);
+	if (img.empty()) {
+		std::cerr << "Could not load image: " << imagePath << std::endl;
+		return 1;
+	}
+
+	cv::imshow("image", img);
+
+	cv::waitKey(0);
+	cv::destroyAllWindows();
 
 	std::cout << "Maze solved!" << std::endl;
 
